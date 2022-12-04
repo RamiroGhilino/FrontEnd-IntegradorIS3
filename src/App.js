facdc4dc832dc3de84e242db32b13bd5c1a290d9
RamiroGhilino <ghilinoramiro@gmail.com>
2022-12-04 18:58:50 -0300

    Agrego cambios para que funcione local o en producción, agrego para verlo estático

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import 'react-awesome-button/dist/styles.css';
 import './App.css';
 
+
 // TODO hacer 1 solo repo con todo
+
 import { useEffect, useState } from 'react';
 
 function App() {
   const [contador, setContador] = useState(0);
-  //const [contador, setContador] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const res = await fetch('http://localhost:5050/');
+      const res = await fetch(process.env.REACT_APP_BACK_URL);
       const obj = await res.json();
       console.log(obj);
       setContador(obj['value']);
@@ -18,29 +19,21 @@ function App() {
   }, []);
 
   const incrementar = async () => {
-    console.log('Incremento');
-    const res = await fetch('http://localhost:5050/add')
+    const res = await fetch(process.env.REACT_APP_BACK_URL + '/add')
     const obj = await res.json();
-    console.log(obj);
-    console.log("Add");
     setContador(obj['value']);
   }
 
   const decrementar = async () => {
-    console.log('Decremento');
-    const res = await fetch('http://localhost:5050/sub')
+    
+    const res = await fetch(process.env.REACT_APP_BACK_URL + '/sub')
     const obj = await res.json();
-    console.log(obj);
-    console.log("Sub");
     setContador(obj['value']);
   }
 
   const reiniciar = async () => {
-    console.log('Reinicio');
-    const res = await fetch('http://localhost:5050/restart')
+    const res = await fetch(process.env.REACT_APP_BACK_URL + '/restart')
     const obj = await res.json();
-    console.log(obj);
-    console.log("Sub");
     setContador(obj['value']);
   }
 
